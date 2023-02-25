08711ee7b222bfc355de4a2c3437bf0d6f8d8334
Knight-Imperitus <120148783+Knight-Imperitus@users.noreply.github.com>
2023-02-24 19:59:18 -0600

    Autonomous, Wrist, and Elevator tuning

---
+++ src/main/java/frc/robot/commands/AutoExtendElevator.java
@@ -0,0 +1,48 @@
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+// package frc.robot.commands;
+
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.SExtendElevator;
+
+// public class AutoExtendElevator extends CommandBase {
+
+
+//   public Double m_ExtendDistance;
+//   // public Double m_ElevatorExtention; 
+  
+
+//   // private final ExtendElevator m_sExtendElevator;
+
+//   /** Creates a new AutoExtendElevator. */
+//   // public AutoExtendElevator(SExtendElevator subsystem, double Extend) {
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     // m_sExtendElevator = subsystem;
+//     // addRequirements(SExtendElevator);
+
+//     m_ExtendDistance = Extend;
+   
+//   }
+
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {
+//     // m_sExtendElevator.SetExtendElevator( m_sExtendElevator);
+//   }
+
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {}
+
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
+
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
